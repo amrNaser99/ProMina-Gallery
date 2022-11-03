46353d6e6fc0464ab10d46a3d948d7137c517611
amr naser <amrnaser99>
2022-11-03 13:54:56 +0200

    upload video preview and apk app and app launcher_icon.png

--- lib/features/home/presentation/auth/login_screen.dart
+++ lib/features/home/presentation/auth/login_screen.dart
@@ -21,148 +21,149 @@ class LoginScreen extends StatelessWidget {
     var size = MediaQuery.of(context).size;
     var contWidth = MediaQuery.of(context).size.width * 0.90;
     return BlocConsumer<AuthCubit, AuthState>(
-  listener: (context, state) {
-    if (state is AuthSuccess && state.authModel != null) {
-
-      Navigator.pushReplacementNamed(context, Routes.home);
-    }
-  },
-  builder: (context, state) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: const AssetImage('assets/images/login_background.png'),
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(
-                Colors.black12.withAlpha(50),
-                BlendMode.darken,
+      listener: (context, state) {
+        if (state is AuthSuccess && state.authModel != null) {
+          Navigator.pushReplacementNamed(context, Routes.home);
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          body: SingleChildScrollView(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: const AssetImage('assets/images/login_background.png'),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    Colors.black12.withAlpha(50),
+                    BlendMode.darken,
+                  ),
+                ),
               ),
-            ),
-          ),
-          child: SizedBox(
-            width: size.width,
-            height: size.height,
-            child: Stack(
-              children: [
-                PositionedDirectional(
-                  top: 0,
-                  start: 0,
-                  child: Image.asset(
-                    'assets/images/path.png',
-                    width: size.width * 0.7,
-                    height: size.height * 0.2,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                PositionedDirectional(
-                  top: size.height * 0.22,
-                  start: 0,
-                  end: 0,
-                  child: Text(
-                    AppStrings.galleryApp,
-                    textAlign: TextAlign.center,
-                    style: getBoldStyle(
-                      fontColor: Colors.black,
-                      fontSize: FontSize.s34,
+              child: SizedBox(
+                width: size.width,
+                height: size.height,
+                child: Stack(
+                  children: [
+                    PositionedDirectional(
+                      top: 0,
+                      start: 0,
+                      child: Image.asset(
+                        'assets/images/path.png',
+                        width: size.width * 0.7,
+                        height: size.height * 0.2,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                ),
-                PositionedDirectional(
-                  start: 0,
-                  end: 0,
-                  top: size.height * 0.3,
-                  child: Padding(
-                    padding: EdgeInsets.all(MediaQuery.of(context).padding.top),
-                    child: GlassBox(
-                      width: contWidth * 0.90,
-                      height: contWidth * 0.90,
-                      child: Center(
-                        child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              Text(
-                                AppStrings.login,
-                                style: getBoldStyle(
-                                    fontColor: Colors.black, fontSize: 30),
-                              ),
-                              const SizedBox(height: 20),
-                              SizedBox(
-                                width: size.width * 0.7,
-                                child: TextField(
-                                  controller:
-                                      AuthCubit.get(context).emailController,
-                                  decoration: const InputDecoration(
-                                    hintText: AppStrings.userName,
-                                    border: OutlineInputBorder(),
+                    PositionedDirectional(
+                      top: size.height * 0.22,
+                      start: 0,
+                      end: 0,
+                      child: Text(
+                        AppStrings.galleryApp,
+                        textAlign: TextAlign.center,
+                        style: getBoldStyle(
+                          fontColor: Colors.black,
+                          fontSize: FontSize.s34,
+                        ),
+                      ),
+                    ),
+                    PositionedDirectional(
+                      start: 0,
+                      end: 0,
+                      top: size.height * 0.3,
+                      child: Padding(
+                        padding:
+                        EdgeInsets.all(MediaQuery.of(context).padding.top),
+                        child: GlassBox(
+                          width: contWidth * 0.90,
+                          height: contWidth * 0.90,
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  Text(
+                                    AppStrings.login,
+                                    style: getBoldStyle(
+                                        fontColor: Colors.black, fontSize: 30),
                                   ),
-                                ),
-                              ),
-                              AppSpaces.vSpace20,
-                              SizedBox(
-                                width: size.width * 0.7,
-                                child: TextField(
-                                  controller:
-                                      AuthCubit.get(context).passwordController,
-                                  obscureText: true,
-                                  decoration: const InputDecoration(
-                                    hintText: AppStrings.password,
-                                    border: OutlineInputBorder(),
-                                  ),
-                                ),
-                              ),
-                              AppSpaces.vSpace20,
-                              SizedBox(
-                                width: size.width * 0.7,
-                                height: 50,
-                                child: ConditionalBuilder(
-                                  condition: state is! AuthLoading ,
-                                  builder: (BuildContext context) {
-                                    return BtnClick(
-                                      text: AppStrings.login,
-                                      color: AppColors.btnColor,
-                                      onPressedButton: () {
+                                  const SizedBox(height: 20),
+                                  SizedBox(
+                                    width: size.width * 0.7,
+                                    child: TextField(
 
-                                        AuthCubit.get(context).login(
-                                          email: AuthCubit.get(context)
-                                              .emailController
-                                              .text
-                                              .trim(),
-                                          password: AuthCubit.get(context)
-                                              .passwordController
-                                              .text
-                                              .trim(),
+                                      controller: AuthCubit.get(context)
+                                          .emailController,
+                                      decoration: const InputDecoration(
+                                        hintText: AppStrings.userName,
+                                        border: OutlineInputBorder(),
+
+                                      ),
+                                    ),
+                                  ),
+                                  AppSpaces.vSpace20,
+                                  SizedBox(
+                                    width: size.width * 0.7,
+                                    child: TextField(
+                                      controller: AuthCubit.get(context)
+                                          .passwordController,
+                                      obscureText: true,
+                                      decoration: const InputDecoration(
+                                        hintText: AppStrings.password,
+                                        border: OutlineInputBorder(),
+                                      ),
+                                    ),
+                                  ),
+                                  AppSpaces.vSpace20,
+                                  SizedBox(
+                                    width: size.width * 0.7,
+                                    height: 50,
+                                    child: ConditionalBuilder(
+                                      condition: state is! AuthLoading,
+                                      builder: (BuildContext context) {
+                                        return BtnClick(
+                                          text: AppStrings.login,
+                                          color: AppColors.btnColor,
+                                          onPressedButton: () {
+                                            AuthCubit.get(context).login(
+                                              email: AuthCubit.get(context)
+                                                  .emailController
+                                                  .text
+                                                  .trim(),
+                                              password: AuthCubit.get(context)
+                                                  .passwordController
+                                                  .text
+                                                  .trim(),
+                                            );
+                                            // Navigator.pushNamed(
+                                            //     context, Routes.home);
+                                          },
                                         );
-                                        Navigator.pushNamed(context, Routes.home);
-
                                       },
-                                    );
-                                  },
-                                  fallback: (BuildContext context) => const Center(
-                                    child: CircularProgressIndicator(),
+                                      fallback: (BuildContext context) =>
+                                      const Center(
+                                        child: CircularProgressIndicator(),
+                                      ),
+                                    ),
                                   ),
-
-                                ),
+                                ],
                               ),
-                            ],
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
-  },
-);
   }
 }
